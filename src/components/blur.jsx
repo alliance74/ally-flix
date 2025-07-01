e38f5202c1621026afed2ce3a2f5e845012dd993
alliance74 <bienvenuealliance@gmail.com>
2025-07-01 22:40:15 +0400

    new changes

--- src/components/blur.jsx
+++ src/components/blur.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 function blur() {
+  
   return (
 <div className="w-16 absolute left-1/3 top-0 h-full z-10 pointer-events-none">
         <div className="w-full h-full bg-gradient-to-r from-gray-900/90 to-transparent backdrop-blur-3xl bg-transpare" />
